e0a56c6842d1062130c9eaa1742e2136558dc0d3
Marek Praski <marekpraski@prgw.com.pl>
2023-01-13 09:06:01 +0100

    w przypadku sukcesu z danych przechwyconych ze strony html tworzony jest obiekt Person. Obiekt ten przekazywany jest dalej do innej metody, w której testuję zamianę go w jsona oraz z jsona z powrotem w nowy obiekt (o innej nazwie); nowy obiekt przekazywany jest dalej, do innej metody, gdzie wyłuskuję z niego parametry logowania; Celem testów jest zasymulowanie operacji na obiekcie utworzonym z danych przechwyconych ze strony html.

--- signup.js
+++ signup.js
@@ -5,6 +5,13 @@ const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+function Person(firstName, lastName, email){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+};
+
+
 app.post("/failure", (req, res)=>{
     res.redirect("/");
 });
@@ -22,14 +29,28 @@ app.post("/", (req, res)=>{
     let s = req.body.floatingSurname;
     let e = req.body.floatingEmail;
     console.log(n + " " + s + " " + e);
-    if(n === "Marek")
+    if(n === "Marek"){
         res.sendFile(__dirname + "/failure.html");
-    else
+    }
+    else{
+        doSomethingBrilliant(new Person(n, s, e));
         res.sendFile(__dirname + "/success.html");
-
+    }
 });
+
+function doSomethingBrilliant(person){
+    let s = JSON.stringify(person);     //zamienia przekazany obiekt w jsona
+    let p2 = JSON.parse(s);         //zamienia jsona w obiekt; typ obiektu nie jest określony ale w zasadzie nie ma to znaczenia
+    savePerson(p2);
+};
+
+function savePerson(p){
+    let n = p.firstName;        // wyciąga z obiektu parametry, tak jakby było wiadomo że jest to obiekt typu Person
+    let s = p.lastName;
+    let e = p.email;
+};
 
 
 app.listen(3000, ()=>{
     console.log("Listening on port 3000");
-})
+});
